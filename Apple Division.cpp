84210680cc410ad9b38f92dfb7258555d3bfa42c
MuscleP <MusclePass@protonnmail.com>
2025-02-24 19:38:35 +0800

    finishing Apple Division

--- Apple Division.cpp
+++ Apple Division.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
-#include <bitset>
-#include <vector>
 
-std::vector<int> p (20, 0);
+int n;
+int p[20];
+long long int sum = 0;
+long long int min_diff = __LONG_LONG_MAX__;
 
-int main()
-{
-    unsigned int n;
-    std::cin >> n;
+inline void update_min_diff(long long int partial_sum) {
+    long long int diff = std::abs((sum - partial_sum) - partial_sum);
+    if (diff < min_diff) min_diff = diff;
+}
 
-    for (unsigned int i = 0; i < n; i++) {
-        std::bitset<8> b1{i};
-
-        for (int i = 0; i < n; i++)
+void dfs(int index, long long int partial_sum) {
+    if (index == n-1) { update_min_diff(partial_sum); }
+    else {
+        dfs(index+1, partial_sum + p[index]);
+        dfs(index+1, partial_sum);
     }
-    std::bitset<8> b1{u}; // binary literal for 42
-        for (std::size_t i = 0; i < b1.size(); ++i) {
-            std::cout << "b1[" << i << "]: " << b1[i] << '\n';
-        }
-    b1[0] = true; // modifies the first bit through bitset::reference
- 
-    std::cout << "After setting bit 0, b1 holds " << b1 << '\n';
+}
+
+int main() {
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        std::cin >> p[i];
+        sum += p[i];
+    }
+    dfs(0, 0);
+    std::cout << min_diff << std::endl;
+    return 0;
 }
